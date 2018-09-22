1f92939cfe7420f89756b88fea557e89bc4b874d
Dhruv Bhanushali <dhruv_b@live.com>
2018-09-23 00:28:43 +0530

    Update the admin registration to denote that it is an MPTT model

--- admin.py
+++ admin.py
@@ -1,6 +1,8 @@
+from mptt.admin import MPTTModelAdmin
+
 from kernel.admin.site import omnipotence
 
 from comments.models import Comment
 
 
-omnipotence.register(Comment)
+omnipotence.register(Comment, MPTTModelAdmin)
