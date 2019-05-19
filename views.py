96703204101db4a5487d57215d95b3b52f7f944e
Dhruv Bhanushali <dhruv_b@live.com>
2019-05-19 10:29:32 +0530

    Fix the logical and idiomatic errors

--- views.py
+++ views.py
@@ -1,37 +1,37 @@
+from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
 
 from comments.models import Comment
 from comments.serializers import CommentSerializer
 
 
-class CommentViewSet(ModelViewSet):
+class CommentViewSet(
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
     """
-    View for CRUD operations of a comment
+    View for CUD operations of a comment
+    Never list or retrieve comments, always serialize in parent model
     """
 
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated, ]
-    http_method_names = [
-        'get',
-        'post',
-        'delete',
-    ]
 
     def get_queryset(self):
         """
-        This function returns the comments corresponding to the user making the
-        request
+        Returns the comments written by the user making the request
         :return: the queryset containing the comments by the user
         """
 
-        return Comment.objects.filter(commenter=self.request.user.person)
+        return Comment.objects.filter(commenter=self.request.person)
 
     def perform_create(self, serializer):
         """
         Add person as the commenter while creating the comment
-        :param serializer: the serializer object
+        :param serializer: the serializer object with the populated data
         """
 
-        person = self.request.user.person
+        person = self.request.person
         serializer.save(commenter=person)
